25446cb93ab47e3beddf214736892fdd466ee545
Ian Melendez <ian.melendez.95@gmail.com>
2026-06-30 15:45:34 -0500

    Initial commit

---
+++ problems/min.js
@@ -0,0 +1,10 @@
+/*
+
+Returns the smaller of its two arguments.
+Works on any combination of strings and numbers.
+
+*/
+
+export default function min(n, m) {
+  // Your code here
+}
---
+++ tests/pick.test.js
@@ -0,0 +1,30 @@
+import { pick } from '../problems';
+
+xdescribe('pick', () => {
+  it('should pick a list of properties out of an object', () => {
+    expect(pick({ a: 1, b: 2, c: 3, d: 4 }, ['b', 'c'])).toEqual({ b: 2, c: 3 });
+    expect(pick({ a: 1, b: 2, c: 3, d: 4 }, [])).toEqual({});
+  });
+
+  it('should ignore keys that do not exist', () => {
+    expect(pick({ a: 1, b: 2, c: 3, d: 4 }, ['a', 'z', 'x'])).toEqual({ a: 1 });
+  });
+
+  it('should not use Array.includes()', () => {
+    // eslint-disable-next-line
+    Array.prototype.includes = jest.fn();
+    pick({ a: 1 }, ['']);
+    expect(Array.prototype.includes).not.toHaveBeenCalled();
+  });
+
+  xit('should throw an error if invalid inputs are given', () => {
+    expect(() => pick()).toThrowError();
+    expect(() => pick(null)).toThrowError();
+    expect(() => pick({}, null)).toThrowError();
+    expect(() => pick(null, [])).toThrowError();
+    expect(() => pick({}, {})).toThrowError();
+    expect(() => pick([], {})).toThrowError();
+    expect(() => pick({})).toThrowError();
+    expect(() => pick(0, null)).toThrowError();
+  });
+});
